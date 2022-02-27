87aa051c7d6702468bbbdcbf2382e1259832448c
Mulac <wallbridge.calum@gmail.com>
2022-02-27 19:00:23 +0000

    move to .py

--- storage.py
+++ storage.py
@@ -1,6 +1,11 @@
 import os
 import boto3
 
+from botocore.exceptions import ClientError
+
+class StorageException(RuntimeError):
+    pass
+
 _s3 = boto3.client('s3')
 
 def _upload(object, bucket, path):
@@ -8,12 +13,14 @@ def _upload(object, bucket, path):
 
 def _download(object, bucket, path):
     if not os.path.exists(path):
-        _s3.download_file(bucket, object, path)
+        try:
+            _s3.download_file(bucket, object, path)
+        except ClientError as e:
+            print(f'FAILED to download {object} from {bucket} to {path}')
     return path
 
 
 def Storage(object, dir="games", bucket="chess-vision", upload=False):
-    path = os.path.join(dir, object)
     if upload:
-        return _upload(object, bucket, path)
-    return _download(object, bucket, path)
+        return _upload(object, bucket, os.path.join(dir, object))
+    return _download(object, bucket, os.path.join(dir, object))
