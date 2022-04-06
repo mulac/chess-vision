fix buffer overflow

chessvision/inference.py
@@ -127,20 +127,6 @@ class LiveInference:
         ).to(self.device)
         return [i for i, occupied in enumerate(self.occupancy_model(squares).argmax(dim=1)) if occupied.item() == 1]
 
-    def occupancy_depth(self, _, depth):
-        board = label.get_board(depth, self.corners)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(board, alpha=0.03), cv2.COLORMAP_JET)
-        square_offset = label.SIZE // 16
-        for i in range(1, 9):
-            for j in range(1, 9):
-                x = label.SIZE * i // 8 - square_offset
-                y = label.SIZE * j // 8 - square_offset
-                sp = (x - label.CUT, y - label.CUT)
-                ep = (x + label.CUT, y + label.CUT)
-                depth_colormap = cv2.rectangle(depth_colormap, sp, ep, (0, 0, 255), 2)
-        self.show_img(depth_colormap, name="depth", size=(500, 500))
-        return label.get_occupied_squares(depth, self.corners)
-
     count = 0
     def print_frame_rate(self):
         self.count += 1
@@ -173,8 +159,9 @@ class LiveInference:
 
 def main(args):
     if args.video:
-        if os.path.exists(args.video): camera = Camera(args.video)
-        else: exit(f"can't read video stream: {args.video} does not exist")
+        camera = Camera(args.video)
+        # if os.path.exists(args.video): camera = Camera(args.video)
+        # else: exit(f"can't read video stream: {args.video} does not exist")
     else:
         camera = RealsenseCamera()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -192,6 +179,7 @@ def main(args):
         occupancy_config,
         device,
         camera,
+        history=20
     )
 
     game.start()

chessvision/interpret.py
@@ -2,14 +2,13 @@
 
 import torch
 import seaborn as sn
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 from functools import reduce
 from operator import mul
 from sklearn.metrics import confusion_matrix
-
-from .label import PIECE_LABELS
 
 
 class Interpreter:
@@ -23,8 +22,15 @@ class Interpreter:
 
     @property
     def _data(self):
+        """ Returns: for each same (input, actual, prediction) """
         for x, y in self.loader:
             yield x.cpu(), y.cpu(), self.model(x.to(self.device)).cpu()
+
+    def losses(self):
+        """ Returns: for each sample (loss, pred, actual, input) """
+        for x, y, o in self._data:
+            for i, loss in enumerate(self.loss_fn(o, y)):
+                yield(loss.item(), o[i].argmax(), y[i], x[i].permute(1, 2, 0))
 
     def accuracy(self):
         correct = 0
@@ -34,16 +40,8 @@ class Interpreter:
         return correct / len(self.loader.dataset) * 100
 
     def plot_top_losses(self, shape=(3, 3)):
-        try:
-            losses = self.losses()
-            n = reduce(mul, shape)
-            losses = sorted(losses, reverse=True)[:n]
-        except RuntimeError as e:
-            for x, y, o in self._data:
-                for i, loss in enumerate(self.loss_fn(o, y)):
-                    print(loss, o[i].argmax(), y[i])
-            print(n)
-            raise
+        losses = sorted(self.losses(), reverse=True)[:reduce(mul, shape)]
+        # losses = torch.topk(self.losses, ac)
         f = plt.figure(figsize=(12, 12))
         f.suptitle("Predicted | Actual | Loss", fontsize=20)
         for i, (loss, pred, actual, img) in enumerate(losses):
@@ -51,12 +49,8 @@ class Interpreter:
             plt.title(f"{self.classes[pred]} | {self.classes[actual]} | {loss:.2f}", fontsize=16)
             plt.imshow(img)
             plt.axis("off")
+        plt.tight_layout()
         return f
-
-    def losses(self):
-        for x, y, o in self._data:
-            for i, loss in enumerate(self.loss_fn(o, y)):
-                yield (loss, o[i].argmax(), y[i], x[i].permute(1, 2, 0))
 
     def plot_confusion_matrix(self):
         cf_values = self.confusion_matrix()
@@ -70,6 +64,7 @@ class Interpreter:
         cf.set_yticklabels(cf.get_ymajorticklabels(), fontsize=40)
         cf.set_ylabel('Ground Truth')
         cf.set_xlabel('Predicted')
+        plt.tight_layout()
         return cf.get_figure()
 
     def confusion_matrix(self):

chessvision/models.py
@@ -99,21 +99,66 @@ class ConvNorm(nn.Module):
 
 
 class ConvRes(models.ResNet):
-    def __init__(self, shape, classes, pretrained=False, use_features=False):
+    def __init__(self, shape, classes, pretrained=False, freeze_features=False):
         if shape[-1] != 3:
             raise ValueError("must have 3 channels")
         super().__init__(models.resnet.BasicBlock, [2, 2, 2, 2])
         if pretrained:
             state_dict = models.resnet.load_state_dict_from_url(models.resnet.model_urls['resnet18'])
             self.load_state_dict(state_dict)
-            if use_features:
-                for param in self.parameters():
-                    param.requires_grad = False
+        if freeze_features:
+            for param in self.parameters():
+                param.requires_grad = False
         self.fc = nn.Sequential(
             nn.Linear(self.fc.in_features, 64),
             nn.ReLU(),
             nn.Linear(64, classes)
         )
+
+    def configure_optimizers(self, config):
+        return torch.optim.AdamW(self.parameters(), 
+            lr=config.learning_rate,
+            weight_decay=config.weight_decay
+        )
+
+
+class MixModel(models.ResNet):
+    def __init__(self, shape, classes, pretrained=False, freeze_features=False):
+        if shape[-1] != 3:
+            raise ValueError("must have 3 channels")
+        super().__init__(models.resnet.BasicBlock, [2, 2, 2, 2])
+        if pretrained:
+            state_dict = models.resnet.load_state_dict_from_url(models.resnet.model_urls['resnet18'])
+            self.load_state_dict(state_dict)
+        if freeze_features:
+            for param in self.parameters():
+                param.requires_grad = False
+        self.color_head = nn.Sequential(
+            nn.Linear(self.fc.in_features, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2) # TODO don't hard code
+        )
+        self.piece_head = nn.Sequential(
+            nn.Linear(self.fc.in_features, 64),
+            nn.ReLU(),
+            nn.Linear(64, 6) # TODO don't hard code
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+
+        return self.color_head(x), self.piece_head(x)
 
     def configure_optimizers(self, config):
         return torch.optim.AdamW(self.parameters(), 

chessvision/train.py
@@ -1,3 +1,4 @@
+import logging
 import torch
 
 from torch.utils.tensorboard import SummaryWriter
@@ -7,35 +8,33 @@ from datetime import datetime
 
 from . import models
 from .game import Game
-from .label import (Labeller, from_id,
-    COLOR_LABELS, PIECE_LABELS, OCCUPIED_LABELS, TYPE_LABELS, 
-    label, label_color, label_occupied, label_type)
+from .label import *
 from .trainer import Trainer, TrainerConfig
 from .interpret import Interpreter
 
 
-EPOCHS = 3
-LR = 0.001
+EPOCHS = 40
+LR = 0.00005
 MOMENTUM = 0.9
 WEIGHT_DECAY = 1e-4
 BATCH_SIZE = 4
-IMG_SIZE = 48
+IMG_SIZE = 96
 AUG_CROP = .9
 AUG_BRIGHTNESS = .75
 AUG_HUE = .1
 CHANNELS = 3
-LABELLER = 'pieces'
+LABELLER = 'type'
 
 labellers = {
-    'pieces': Labeller(PIECE_LABELS, label, [from_id(i).unicode_symbol() for i in range(len(PIECE_LABELS))]),
+    'piece':    Labeller(PIECE_LABELS, label, [from_id(i).unicode_symbol() for i in range(len(PIECE_LABELS))]),
     'occupied': Labeller(OCCUPIED_LABELS, label_occupied, ["Occupied", "Empty"]),
-    'color': Labeller(COLOR_LABELS, label_color, ["White", "Black"]),
-    'type': Labeller(TYPE_LABELS, label_type, ["pawn", "knight", "bishop", "rook", "queen", "king"])
+    'color':    Labeller(COLOR_LABELS, label_color, ["White", "Black"]),
+    'type':     Labeller(TYPE_LABELS, label_type, ["pawn", "knight", "bishop", "rook", "queen", "king"]),
 }
 
 config = TrainerConfig(
-    # train_folder = '/tmp/chess-vision-2v49ovvp',
-    # test_folder = '/tmp/chess-vision-5pqhxnnj',
+    # train_folder = '/tmp/chess-vision-h23992mn',
+    # test_folder = '/tmp/chess-vision-x3pka21l',
     train_games = (
         Game("Adams", 1),
         Game("Adams", 2),
@@ -54,8 +53,8 @@ config = TrainerConfig(
     momentum = MOMENTUM,
     channels = CHANNELS,
     image_shape = torch.tensor((IMG_SIZE, IMG_SIZE, CHANNELS)),
-    classes = labellers[LABELLER].labels,
-    label_fn = labellers[LABELLER].label_fn,
+    labeller = labellers[LABELLER],
+    # loss_fn = lambda pred, actual: torch.nn.CrossEntropyLoss(pred[0], actual[:,0]) + torch.nn.CrossEntropyLoss(pred[1], actual[:,0]),
     transform = transforms.Compose([
         transforms.Resize(IMG_SIZE),
         # transforms.Grayscale(),
@@ -78,26 +77,26 @@ config = TrainerConfig(
     ])
 )
 
-print(config)
+logging.info(config)
 
 trainer = Trainer(
-    models.ConvRes(config.image_shape, len(config.classes), pretrained=True),
+    models.ConvRes(config.image_shape, len(config.labeller.classes), pretrained=True),
     config,
     SummaryWriter(f"runs/chess-vision_{datetime.now().strftime('%Y%m%d_%H%M%S')}")
 )
 
-print("\nBegin training...")
+logging.info("Begin training...")
 trainer.train()
 
-print("\nEvaluating...")
+logging.info("Evaluating...")
 interp = Interpreter(
     model=torch.load("model"), 
     loader=DataLoader(trainer.test_dataset, batch_size=100, num_workers=4),
     loss_fn=config.loss_fn,
-    classes=labellers[LABELLER].names
+    classes=config.labeller.names
 )
 
-print(f"Accuracy: {interp.accuracy():.2f}")
+logging.info(f"Accuracy: {interp.accuracy():.2f}")
 trainer.writer.add_figure("Confusion Matrix", interp.plot_confusion_matrix())
-trainer.writer.add_figure("Top Losses", interp.plot_top_losses((6, 6)))
+trainer.writer.add_figure("Top Losses", interp.plot_top_losses((4, 4)))
 trainer.writer.close()

chessvision/trainer.py
@@ -4,13 +4,15 @@ import os
 import torch
 import numpy as np
 
-from typing import Tuple, Callable, Any, List
+from typing import Tuple, Callable
 from tqdm import trange
 from dataclasses import dataclass
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from .game import Game, LabelOptions, save_games
+
+from .game import Game, LabelOptions, Labeller, save_games
+
 
 class ChessFolder(datasets.ImageFolder):
     def find_classes(self, dir):
@@ -39,9 +41,8 @@ class TrainerConfig:
     momentum: float = 0.9
     epochs: int = 300
     batch_size: int = 4
-    classes: List[Any] = None
+    labeller: Labeller = None
     image_shape: int = None
-    label_fn: Callable = None
     loss_fn: Callable = torch.nn.CrossEntropyLoss()
     train_folder: str = None
     test_folder: str = None
@@ -56,8 +57,8 @@ class Trainer:
         self.config = config
         self.writer = writer
 
-        train_folder = config.train_folder if config.train_folder else save_games(config.train_games, config.label_fn, config.classes)
-        test_folder = config.test_folder if config.train_folder else save_games(config.test_games, config.label_fn, config.classes)
+        train_folder = config.train_folder if config.train_folder else save_games(config.train_games, config.labeller.label_fn, config.labeller.classes)
+        test_folder = config.test_folder if config.train_folder else save_games(config.test_games, config.labeller.label_fn, config.labeller.classes)
         self.train_dataset = ChessFolder(root=train_folder, transform=config.transform)
         self.test_dataset = ChessFolder(root=test_folder, transform=config.infer_transform)
 